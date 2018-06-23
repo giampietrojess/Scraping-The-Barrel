9fd2bccb83d8c7a0ea335a44e9710f646e72aace
Jess Giampietro <giampietrojess@gmail.com>
2018-06-22 23:15:09 -0400

    change website

--- server.js
+++ server.js
@@ -1,41 +1,64 @@
-//dependencies
+// Node Dependencies
+var express = require('express');
+var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var logger = require('morgan');
 
-//initialize Express app
-var express = require('express');
+var logger = require('morgan'); // for debugging
+var request = require('request'); // for web-scraping
+var cheerio = require('cheerio'); // for web-scraping
+
+
+// Initialize Express for debugging & body parsing
 var app = express();
-
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
   extended: false
-}));
+}))
 
+// Serve Static Content
 app.use(express.static(process.cwd() + '/public'));
 
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
+// Express-Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
+// Database Configuration with Mongoose
+// ---------------------------------------------------------------------------------------------------------------
+
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/3000";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to Mongoose!')
+
+// Show any Mongoose errors
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
 });
 
-var routes = require('./controller/controller.js');
-app.use('/', routes);
+// Once logged in to the db through mongoose, log a success message
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
 
+// Import the Comment and Article models
+var Comment = require('./models/Comment.js');
+var Article = require('./models/Article.js');
+
+
+// Import Routes/Controller
+var router = require('./controllers/controller.js');
+app.use('/', router);
+
+
+// Launch App
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
-  console.log('Listening on PORT ' + port);
+  console.log('Running on port: ' + port);
 });
