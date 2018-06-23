049a3f6092359bfece2a75ada510a730dc2c3eb2
Jess Giampietro <giampietrojess@gmail.com>
2018-06-22 23:15:20 -0400

    adjust to make work

--- models/Article.js
+++ models/Article.js
@@ -1,23 +1,49 @@
+// Include the momentJS library
+var moment = require("moment");
+
+// Require Mongoose
 var mongoose = require('mongoose');
 
+// Create a Schema Class
 var Schema = mongoose.Schema;
 
+// Create Article Schema
 var ArticleSchema = new Schema({
+
+  // Title of Article
   title: {
     type: String,
     required: true
   },
+
+  // Link to Article
   link: {
     type: String,
     required: true
   },
-  comment: [{
+  
+  // Summary of Article
+  summary: {
+    type: String,
+    required: true
+  },
+
+  // Date of article scrape (saving as a string to pretify it in Moment-JS)
+  updated: {
+    type: String,
+    default: moment().format('MMMM Do YYYY, h:mm A')
+  },
+
+  // Create a relation with the Comment model
+  comments: [{
     type: Schema.Types.ObjectId,
-    ref: "Comment"
+    ref: 'Comment'
   }]
+
 });
 
-var Article = mongoose.model("Article", ArticleSchema);
+// Create the Article model with Mongoose
+var Article = mongoose.model('Article', ArticleSchema);
 
-//export model
+// Export the Model
 module.exports = Article;
--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,26 @@
+// Require Mongoose
 var mongoose = require('mongoose');
 
+// Create a Schema Class
 var Schema = mongoose.Schema;
 
+// Create Comment Schema
 var CommentSchema = new Schema({
-  name: {
+
+  // Author's Name
+  author: {
     type: String
   },
-  body: {
-    type: String,
-    required: true
+  // Comment Content
+  content: {
+    type: String
   }
+  
 });
 
-var Comment = mongoose.model("Comment", CommentSchema);
 
+// Create the Comment model with Mongoose
+var Comment = mongoose.model('Comment', CommentSchema);
+
+// Export the Model
 module.exports = Comment;
