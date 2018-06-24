83fa156dbfa6771add9577755d3c12653f9b6da6
Jess Giampietro <giampietrojess@gmail.com>
2018-06-24 01:55:31 -0400

    establish comment schema

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,20 @@
 // Require Mongoose
 var mongoose = require('mongoose');
 
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var CommentSchema = new Schema({
+  name: {
+    type: String
+  },
+  body: {
+    type: String,
+    required: true
+  }
+});
+
+var Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
